a350149eccda30e717e59d5bbe66ca490ce0dd4b
VasilPaligorov <vpaligorov@gmail.com>
2022-08-31 18:18:39 +0300

    Edit: Css now works with display: flex

--- task1-search-band/src/App.js
+++ task1-search-band/src/App.js
@@ -1,14 +1,20 @@
-import Form from 'react-bootstrap/Form';
 import SearchOptions from './searchOptions';
 import {useState } from 'react';
-import './css/App.css'
+import './css/App.css';
+import {getData} from './service/getData';
 
 export default function App() {
   const [initElements, setElements] = useState(['A', 'B', 'C', 'D', 'E']);
 
-  return (
+  async function LoadElements(band) {
+    let data = await getData(band);
+    setElements(initElements.concat(data))
+    console.log(data);
+  }
+
+  return (  
     <div className="App" >
-      <Form.Control id='form' type='text' placeholder="Search Band" />
+      <input id='form' type='text' onChange={(e)=>LoadElements(e.target.value)} placeholder="Search Band" />
       <SearchOptions initElements={initElements} setElements={setElements}/>
     </div>
   );
--- task1-search-band/src/service/getData.js
+++ task1-search-band/src/service/getData.js
@@ -1,5 +1,5 @@
-import React from 'react'
-
-export default function getData() {
-  
+export function getData(band) {
+   return fetch('https://itunes.apple.com/search?term='+band)
+    .then((response) => response.json())
+    .then((data) => [... new Set(data.results.map(item=>item.collectionName))].sort().slice(0,5)); 
 }
