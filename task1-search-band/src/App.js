3f8ff91e52e292a844aeda6ba74df001254c5e0a
Vasil Paligorov <vpaligorov@Vasils-MacBook-Air.local>
2022-09-02 15:07:30 +0300

    Add: Current stage of the project

--- task1-search-band/src/App.js
+++ task1-search-band/src/App.js
@@ -1,21 +1,11 @@
 import SearchOptions from './searchOptions';
-import {useState } from 'react';
 import './css/App.css';
-import {getData} from './service/getData';
+
 
 export default function App() {
-  const [initElements, setElements] = useState(['A', 'B', 'C', 'D', 'E']);
-
-  async function LoadElements(band) {
-    let data = await getData(band);
-    setElements(initElements.concat(data))
-    console.log(data);
-  }
-
   return (  
     <div className="App" >
-      <input id='form' type='text' onChange={(e)=>LoadElements(e.target.value)} placeholder="Search Band" />
-      <SearchOptions initElements={initElements} setElements={setElements}/>
+      <SearchOptions/>
     </div>
   );
 }
--- task1-search-band/src/searchOptions.js
+++ task1-search-band/src/searchOptions.js
@@ -1,28 +1,45 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
+import { getData } from './service/getData';
 
 
-export default function SearchOptions({ initElements, setElements }) {
-
+export default function SearchOptions() {
     const [intervalId, setIntervalId] = useState();
+    const [initElements, setElements] = useState(['A', 'B', 'C', 'D', 'E']);
 
     useEffect(() => {
         console.log(1);
-        setIntervalId(setInterval(()=>interval(), 2000));
+        setIntervalId(setInterval(interval, 2000));
     }, []);
 
-    const interval = () => {
+    async function LoadElements(band) {
+        let data = await getData(band);
+        clearInterval(intervalId);
+        setElements([...initElements, ...data]);
+        setIntervalId(setInterval(interval, 2000));
+    }
+
+    function interval () {
         let option = initElements[0];
-        let newOptions = initElements; // za domashno
-        newOptions.push(option);
+        let newOptions = initElements;
+        if (initElements.length <= 5)
+            newOptions.push(option);
         newOptions.shift();
-        setElements([...newOptions]);
+        setElements([...initElements]);
     }
 
 
     return (
-        <div id="search-options">
-            {initElements.map((element, index) => <p key={index}>{element}</p>)}
-        </div>
+        <>
+            <input id='form' type='text' placeholder="Search Band" onInput={(e)=>LoadElements(e.target.value)} />
+            <div id="search-options">
+                {initElements.map((element, index) =>
+                    index <= 4
+                        ? <p key={index}>{element}</p>
+                        : null
+                )}
+            </div>
+        </>
+
     )
 }
